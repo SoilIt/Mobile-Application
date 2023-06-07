88e702bc94b4dc006ee2c2aae51aa25561fbb10e
Aisy Al Fawwaz <59833758+aisyalfawwaz@users.noreply.github.com>
2023-06-07 15:27:07 +0700

    fix

---
+++ app/src/main/java/com/github/user/soilitouraplication/database/HistoryDatabase.kt
@@ -0,0 +1,31 @@
+package com.github.user.soilitouraplication.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.github.user.soilitouraplication.api.History
+
+@Database(entities = [History::class], version = 1)
+abstract class HistoryDatabase : RoomDatabase() {
+    abstract fun historyDao(): HistoryDao
+
+    companion object {
+        private const val DATABASE_NAME = "app_database"
+
+        @Volatile
+        private var INSTANCE: HistoryDatabase? = null
+
+        fun getInstance(context: Context): HistoryDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HistoryDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/github/user/soilitouraplication/ui/history/HistoryAdapter.kt
@@ -0,0 +1,54 @@
+package com.github.user.soilitouraplication.ui.history
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.user.soilitouraplication.api.History
+import com.github.user.soilitouraplication.databinding.HistoryItemBinding
+import com.github.user.soilitouraplication.utils.DateUtils
+
+class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+    private var historyList: List<History> = listOf()
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(historyList: List<History>) {
+        this.historyList = historyList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            HistoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val historyItem = historyList[position]
+        holder.bind(historyItem)
+    }
+
+    override fun getItemCount(): Int {
+        return historyList.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(list: List<History>) {
+        historyList = list
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: HistoryItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(historyItem: History) {
+            binding.apply {
+                tvSoil.text = historyItem.soil_type
+                tvDateCreated.text = DateUtils.formatDateTime(historyItem.created_at)
+                tvMoisture.text = historyItem.soil_moisture.toString()
+                tvSoilCondition.text = historyItem.soil_condition
+            }
+            // Bind data ke tampilan ViewHolder sesuai kebutuhan
+            // Contoh: binding.textView.text = historyItem.nama
+        }
+    }
+}
